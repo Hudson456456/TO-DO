8777f4b412fba0c1522620957ccbbbbbf49fabfb
Hudson <hudsonavila109@gmail.com>
2026-06-18 22:13:05 -0300

    falta por put e delet

---
+++ scr/data/memory.js
@@ -0,0 +1,9 @@
+const todos = [
+    {id: 1, titulo: "Estudar Node.js", feito: false},
+    {id: 2, titulo: "Fazer exercícios", feito: false},
+    {id: 3, titulo: "Revisar código", feito: false}
+];
+
+module.exports = {
+    todos
+}
