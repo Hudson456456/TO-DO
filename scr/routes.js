eca508ea64d83962a30c27c0566b1c028d0899b8
Hudson <hudsonavila109@gmail.com>
2026-06-30 22:09:11 -0300

    terminamos o dbeaver e tudo

--- scr/routes.js
+++ scr/routes.js
@@ -2,6 +2,69 @@ const express = require("express");
 const connectDB = require("./data/database");
 const router = express.Router();
 
+router.put("/todos/:id", async (req, res) => {
+
+    const id = Number(req.params.id);
+    const { titulo, feito } = req.body;
+
+    const db = await connectDB();
+
+    const tarefa = await db.get(
+        "SELECT * FROM tarefas WHERE id = ?",
+        [id]
+    );
+
+    if (!tarefa) {
+        return res.status(404).json({
+            mensagem: "Tarefa não encontrada"
+        });
+    }
+
+    await db.run(
+        "UPDATE tarefas SET titulo = ?, feito = ? WHERE id = ?",
+        [titulo, feito, id]
+    );
+
+    const tarefaAtualizada = await db.get(
+        "SELECT * FROM tarefas WHERE id = ?",
+        [id]
+    );
+
+    return res.status(200).json({
+        mensagem: "Tarefa atualizada com sucesso",
+        tarefa: tarefaAtualizada
+    });
+
+});
+
+router.delete("/todos/:id", async (req, res) => {
+
+    const id = Number(req.params.id);
+
+    const db = await connectDB();
+
+    const tarefa = await db.get(
+        "SELECT * FROM tarefas WHERE id = ?",
+        [id]
+    );
+
+    if (!tarefa) {
+        return res.status(404).json({
+            mensagem: "Tarefa não encontrada"
+        });
+    }
+
+    await db.run(
+        "DELETE FROM tarefas WHERE id = ?",
+        [id]
+    );
+
+    return res.status(200).json({
+        mensagem: "Tarefa removida com sucesso"
+    });
+
+});
+
 
 // LISTAR TODAS AS TAREFAS
 router.get("/todos", async (req, res) => {
